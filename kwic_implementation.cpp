b360c3a90d354b823845d83e2c1d9f0d61e8f369
Cynthia Marie <cynthia.rivera15@upr.edu>
2024-09-24 17:35:48 -0500

    Finished Input and Line_Storage classes
    Added all the methods required for both Line_Storage an Input... is not presenting any errors, when compiling, just warnings. However, we still need to check if it works how is supposed to

--- kwic_implementation.cpp
+++ kwic_implementation.cpp
@@ -6,16 +6,158 @@ Course: SE6362
 #include <iostream>
 #include <fstream>
 #include <string>
-#include<list>
+#include <string.h>
+#include <vector>
 
 using namespace std;
 
-class Master_Control {
+class Line_Storage {
+  public:
+    string fullLine;
+    vector<string> storedLines;
+    vector<int> numWords;
+
+    void Set_Char(int line, int word, int chrc, char p) {
+      Line_Storage::storedLines[line] += p;
+    }
+
+    char Get_Char(int line, int word, int chrc) {
+      char p;
+      int spaces = 1;
+      int counter = 0;
+
+      while(spaces < word) {
+        p = Line_Storage::storedLines[line - 1][counter];
+
+        if(p == ' ') {
+          spaces++;
+        }
+
+        counter++;
+      }
+
+      while(chrc != 0) {
+        p = Line_Storage::storedLines[line - 1][counter];
+
+        chrc--;
+        counter++;
+      }
+
+      return p;
+    }
+
+    int Get_Word(int line) {
+      return Line_Storage::numWords[line - 1];
+    }
+};
+
+class Input {
+  public:
+    string fileName;
+
+    void Read_File(Line_Storage readLine) {
+      // Opens input file
+      ifstream file(fileName);
+
+      // Checks if file has been opened
+      if (!file.is_open()) {
+        cout << "Error opening the file!" << endl;
+        exit(1);
+      }
+
+      //Read each line of the file and store it in the vector
+      string line;
+
+      while (getline(file, line)) {
+        readLine.fullLine = line;
+      }
+
+      // Close the file
+      file.close();
+    }
+
+    void Call_Line_Storage_Set_Char(Line_Storage readLine) {
+      int word = 1;
+      int counter = 0;
+      readLine.storedLines.push_back("");
+
+      for (int i = 0; i < readLine.fullLine.length(); i++) {
+        if (readLine.fullLine[i] == '$') {
+          counter++;
+          readLine.storedLines.push_back("");
+          readLine.numWords.push_back(word);
+          word = 1;
+        }
+        else {
+          readLine.Set_Char(counter, word, i, readLine.fullLine[i]);
+
+          if (i == ' ') {
+            word++;
+          }
+        }
+      }
+
+    }
+};
+
+class Circular_Shift {
   private:
-    string inputFileName;
-    string outputFileName = "kwic_output.txt";
+    void Set_Char(int shift, int word, int chrc, char p) {
+
+    }
 
   public:
+    vector<vector<string> > shiftedLines;
+
+    string Get_Char(int shift, int word, string chrc) {
+
+    }
+
+    int Get_Word(int shift) {
+
+    }
+
+    string Setup (Line_Storage readLine) {
+
+    }
+};
+
+class Alphabetizer {
+  private:
+    void Set_Char(int alpha, int word, int chrc, char p) {
+
+    }
+
+  public:
+    vector<vector<string> > alphabetizedLines;
+
+    string Get_Char(int alpha, int word, string chrc) {
+
+    }
+
+    string Get_Word(int alpha) {
+
+    }
+
+    void Merge_Sort() {
+
+    }
+};
+
+class Output {
+  public:
+    void Display_Lines() {
+
+    }
+};
+
+class Master_Control {
+  private:
+    const string outputFileName = "kwic_output.txt";
+
+  public:
+    string inputFileName;
+
     Input inputFile;
     Line_Storage readLine;
     Circular_Shift shiftedLine;
@@ -27,10 +169,8 @@ class Master_Control {
       inputFile.fileName = Master_Control::inputFileName;
 
       inputFile.Read_File(readLine);
-    }
 
-    void Call_Line_Storage() {
-
+      inputFile.Call_Line_Storage_Set_Char(readLine);
     }
 
     void Call_Circular_Shift() {
@@ -46,99 +186,13 @@ class Master_Control {
     }
 };
 
-class Input {
-  public:
-    string fileName;
-
-    Read_File(readLine) {
-      // Opens input file
-      ifstream file(fileName);
-
-      // Checks if file has been opened
-      if (!file.is_open()) {
-        cerr << "Error opening the file!" << endl;
-        return 1;
-      }
-
-      //Read each line of the file and store it in the vector
-      string line;
-
-      while (getline(file, line)) {
-        readLine = line;
-      }
-
-      // Close the file
-      readLine.storedLines = file.close();
-    }
-};
-
-class Line_Storage {
-  public:
-    string storedLines;
-
-    Set_Char(line, word, char, p) {
-
-    }
-
-    Get_Char(line, word, p) {
-
-    }
-
-    Get_Word(line) {
-
-    }
-};
-
-class Circular_Shift {
-  public:
-    Setup () {
-
-    }
-
-    Set_Char(shift, word, char, p) {
-
-    }
-
-    Get_Char(shift, word, char) {
-
-    }
-
-    Get_Word(shift) {
-
-    }
-};
-
-class Alphabetizer {
-  public:
-    Set_Char(line, word, char, p) {
-
-    }
-
-    Get_Char(line, word, char) {
-
-    }
-
-    Get_Word(line, word, char) {
-
-    }
-};
-
-class Output {
-  public:
-    Display_Lines(line) {
-
-    }
-};
-
-int main(int argc, string argv) {
+int main(int argc, char *argv[]) {
   if (argc == 1) {
     Master_Control kwic;
 
-    kwic.inputFileName = argv;
+    kwic.inputFileName = argv[0];
 
     kwic.Call_Input();
-
-    kwic.Call_Line_Storage();
 
     kwic.Call_Circular_Shift();
 
